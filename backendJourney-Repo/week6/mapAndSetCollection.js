7d766f35d1b14099353a983eb68a2088a3a83676
Joy Donatus <donatus.joy@outlook.com>
2026-04-05 20:35:54 +0100

    working further  on maps and set and week7 assignment

--- backendJourney-Repo/week6/mapAndSetCollection.js
+++ backendJourney-Repo/week6/mapAndSetCollection.js
@@ -2,3 +2,25 @@
 // implement word frequency tracking, build a phone book, 
 // remove duplicates with Set, find unique characters, 
 // and implement set operations (union, intersection, difference)
+
+//Create a Map to store users:
+
+// Add 3 users (id → name)
+// Retrieve one user
+// Check if a user exists
+
+const user = new Map();
+console.log(user);
+
+user.set(1, { name: "Joy", age: "50", address: {streetNumber: 40, city: "Lagos", Country: "Nigeria"}});
+user.set(2, { name: "Mike", age: "20", address: {streetNumber: 30, city: "Abuja", Country: "Namibia"}});
+user.set(3, { name: "Steve", age: "10"});
+
+
+console.log(user.get(1).name);
+
+
+
+// user.push(user1);
+// user.push(user2);
+// user.push(user3)
