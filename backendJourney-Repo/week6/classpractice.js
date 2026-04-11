72cc126d3d12a0696aea5b2edaba130a463a4b8a
Joy Donatus <donatus.joy@outlook.com>
2026-04-11 16:03:00 +0100

    updated my assignment

--- backendJourney-Repo/week6/classpractice.js
+++ backendJourney-Repo/week6/classpractice.js
@@ -22,3 +22,35 @@ const newProducts = products.map((product) => {
 }) 
 
 console.log(newProducts);
+
+//Create a user cache with Map
+const user = new Map();
+console.log(user);
+
+// Add 3 users (id → name)
+user.set(1, { name: "Joy", id: 1, age: "50", address: {streetNumber: 40, city: "Lagos", Country: "Nigeria"}});
+user.set(2, { name: "Mike", id: 2, age: "20", address: {streetNumber: 30, city: "Abuja", Country: "Namibia"}});
+user.set(3, { name: "Steve", id: 3, age: "10"});
+
+// Retrieve one user
+console.log(user.get(1));
+console.log(user.get(2).address.city);
+console.log(user.get(3).age);
+
+console.log(user.size);
+
+for (let [a, b] of user) {
+  console.log(a, b);
+}
+
+// Check if a user exists
+console.log(user.has(1));
+
+
+//deleting a user
+user.delete(1);
+console.log(user);
+
+//emptying the user object
+user.clear();
+console.log(user);
--- backendJourney-Repo/week6/mapAndSetCollection.js
+++ backendJourney-Repo/week6/mapAndSetCollection.js
@@ -3,24 +3,61 @@
 // remove duplicates with Set, find unique characters, 
 // and implement set operations (union, intersection, difference)
 
-//Create a Map to store users:
 
-// Add 3 users (id → name)
-// Retrieve one user
-// Check if a user exists
 
+//Create a user cache with Map
 const user = new Map();
 console.log(user);
 
-user.set(1, { name: "Joy", age: "50", address: {streetNumber: 40, city: "Lagos", Country: "Nigeria"}});
-user.set(2, { name: "Mike", age: "20", address: {streetNumber: 30, city: "Abuja", Country: "Namibia"}});
-user.set(3, { name: "Steve", age: "10"});
+user.set(1, { name: "Joy", id: 1, age: "50", address: {streetNumber: 40, city: "Lagos", Country: "Nigeria"}});
+user.set(2, { name: "Mike", id: 2, age: "20", address: {streetNumber: 30, city: "Abuja", Country: "Namibia"}});
+user.set(3, { name: "Steve", id: 3, age: "10"});
 
 
-console.log(user.get(1).name);
+console.log(user);
 
 
+//Word Frequency Counter
 
-// user.push(user1);
-// user.push(user2);
-// user.push(user3)
+const sentence = "apple banana apple orange banana apple";
+
+function frequencyCounter(sentences) {
+  const words = sentences.split(" ");
+  const newSentence = new Map();
+  words.map((word) => {
+    let newWord = newSentence.get(word) || 0;
+    newSentence.set(word, newWord + 1);
+  });
+  return newSentence;
+
+}
+
+console.log(frequencyCounter(sentence));
+
+//build a phone book
+const phoneBook = new Map();
+
+phoneBook.set(1, { name: "Joy", phoneNumber: "+2347905678"});
+phoneBook.set(2, { name: "Peter", phoneNumber: "+2347905679"});
+phoneBook.set(3, { name: "Jeniffer", phoneNumber: "+2347905674"});
+
+console.log(phoneBook);
+
+//remove duplicates with Set
+const alphabets = ["a", "b", "c", "b", "c", "d", "z", "h"]
+const removeDuplicateAlphabets = new Set(alphabets);
+console.log(removeDuplicateAlphabets);
+
+//find unique characters
+const story = "Story, story";
+const uniqueCharacters = new Set(story);
+console.log(uniqueCharacters);
+
+
+//set operations (union, intersection, difference)
+const firstSet = new Set([1, 2, 3, 4]);
+const secondSet = new Set([3, 4, 5, 6]);
+
+//union (merging two sets with spread operator)
+const newSet = new Set([...firstSet, ...secondSet]);
+console.log(newSet);
