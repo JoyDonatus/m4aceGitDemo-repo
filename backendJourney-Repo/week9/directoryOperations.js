a3b9de1e1afd9cb3fc456cf3db3d7d0217f045a2
Joy Donatus <donatus.joy@outlook.com>
2026-04-26 18:35:06 +0100

    week9 assignment folder and file creation

---
+++ backendJourney-Repo/week9/directoryOperations.js
@@ -0,0 +1,5 @@
+//Directory Operations - Create directories, 
+// read directory contents, 
+// delete directories recursively, 
+// find files by extension, 
+// and generate directory tree structures
