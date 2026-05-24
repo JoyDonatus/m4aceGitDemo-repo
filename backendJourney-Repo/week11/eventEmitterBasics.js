d49823e89891cae22fe4a2c76d9e678c93e91925
Joy Donatus <donatus.joy@outlook.com>
2026-05-24 19:24:22 +0100

    created week13 assignment folder and files

--- backendJourney-Repo/week11/eventEmitterBasics.js
+++ backendJourney-Repo/week11/eventEmitterBasics.js
@@ -1,1 +1,186 @@
-//Event Emitter Basics - Create a custom UserManager class with event emitters for user operations (add, update, remove), implement event listeners and one-time listeners
+//Event Emitter Basics - 
+// Create a custom UserManager class with event emitters for user operations (add, update, remove), 
+const EventEmitter = require("events");
+
+// Create UserManager class
+class UserManager extends EventEmitter {
+  constructor() {
+    super();
+    this.users = [];
+  }
+
+  // Add user
+  addUser(user) {
+    this.users.push(user);
+
+    // Emit event
+    this.emit("add", user);
+  }
+
+  // Update user
+  updateUser(id, newData) {
+    const user = this.users.find((u) => u.id === id);
+
+    if (user) {
+      Object.assign(user, newData);
+
+      // Emit event
+      this.emit("update", user);
+    } else {
+      console.log("User not found");
+    }
+  }
+
+  // Remove user
+  removeUser(id) {
+    const index = this.users.findIndex((u) => u.id === id);
+
+    if (index !== -1) {
+      const removedUser = this.users.splice(index, 1)[0];
+
+      // Emit event
+      this.emit("remove", removedUser);
+    } else {
+      console.log("User not found");
+    }
+  }
+}
+
+// Create object
+const userManager = new UserManager();
+
+// Listen for add event
+userManager.on("add", (user) => {
+  console.log("User added:", user);
+});
+
+// Listen for update event
+userManager.on("update", (user) => {
+  console.log("User updated:", user);
+});
+
+// Listen for remove event
+userManager.on("remove", (user) => {
+  console.log("User removed:", user);
+});
+
+// Test the class
+userManager.addUser({
+  id: 1,
+  name: "John",
+  age: 25,
+});
+
+userManager.updateUser(1, {
+  age: 26,
+});
+
+userManager.removeUser(1);
+
+// implement event listeners and one-time listeners
+    const EventEmitter = require("events");
+
+// Create UserManager class
+class UserManager extends EventEmitter {
+  constructor() {
+    super();
+    this.users = [];
+  }
+
+  // Add user
+  addUser(user) {
+    this.users.push(user);
+
+    // Emit add event
+    this.emit("add", user);
+  }
+
+  // Update user
+  updateUser(id, newData) {
+    const user = this.users.find((u) => u.id === id);
+
+    if (user) {
+      Object.assign(user, newData);
+
+      // Emit update event
+      this.emit("update", user);
+    } else {
+      console.log("User not found");
+    }
+  }
+
+  // Remove user
+  removeUser(id) {
+    const index = this.users.findIndex((u) => u.id === id);
+
+    if (index !== -1) {
+      const removedUser = this.users.splice(index, 1)[0];
+
+      // Emit remove event
+      this.emit("remove", removedUser);
+    } else {
+      console.log("User not found");
+    }
+  }
+}
+
+// Create object
+const userManager = new UserManager();
+
+
+// =========================
+// EVENT LISTENERS (.on)
+// =========================
+
+// Runs every time a user is added
+userManager.on("add", (user) => {
+  console.log("User added:", user);
+});
+
+// Runs every time a user is updated
+userManager.on("update", (user) => {
+  console.log("User updated:", user);
+});
+
+// Runs every time a user is removed
+userManager.on("remove", (user) => {
+  console.log("User removed:", user);
+});
+
+
+// =========================
+// ONE-TIME LISTENER (.once)
+// =========================
+
+// Runs ONLY ONE TIME
+userManager.once("add", (user) => {
+  console.log("This runs only once:");
+  console.log("First user added is", user.name);
+});
+
+
+// =========================
+// TESTING
+// =========================
+
+// First add
+userManager.addUser({
+  id: 1,
+  name: "John",
+  age: 25,
+});
+
+// Second add
+userManager.addUser({
+  id: 2,
+  name: "Mary",
+  age: 22,
+});
+
+// Update user
+userManager.updateUser(1, {
+  age: 26,
+});
+
+// Remove user
+userManager.removeUser(2);
--- backendJourney-Repo/week11/realWorldProject.js
+++ backendJourney-Repo/week11/realWorldProject.js
@@ -1,1 +1,75 @@
-//Real-World Project: Log Processor - Build a log file processor using streams and events to parse, filter, and analyze log files efficiently
+//Real-World Project: Log Processor - 
+// Build a log file processor using streams and events to parse, filter, and analyze log files efficiently
+
+const fs = require("fs");
+const readline = require("readline");
+const EventEmitter = require("events");
+
+// Create custom class
+class LogProcessor extends EventEmitter {
+  constructor(filePath) {
+    super();
+
+    this.filePath = filePath;
+    this.errorCount = 0;
+  }
+
+  // Start processing logs
+  processLogs() {
+
+    // Create read stream
+    const stream = fs.createReadStream(this.filePath);
+
+    // Read line by line
+    const rl = readline.createInterface({
+      input: stream,
+      crlfDelay: Infinity,
+    });
+
+    // When a new line is read
+    rl.on("line", (line) => {
+
+      // Emit event for every line
+      this.emit("lineRead", line);
+
+      // Filter ERROR logs
+      if (line.includes("ERROR")) {
+
+        this.errorCount++;
+
+        // Emit error event
+        this.emit("errorLog", line);
+      }
+    });
+
+    // When reading is complete
+    rl.on("close", () => {
+
+      // Emit complete event
+      this.emit("done", this.errorCount);
+    });
+  }
+}
+
+// Create object
+const processor = new LogProcessor("log.txt");
+
+// Runs for every line
+processor.on("lineRead", (line) => {
+  console.log("Line Read:", line);
+});
+
+// Runs only for ERROR logs
+processor.on("errorLog", (line) => {
+  console.log("ERROR FOUND:", line);
+});
+
+// Runs when processing finishes
+processor.on("done", (count) => {
+  console.log("\nLog processing complete");
+  console.log("Total Errors:", count);
+});
+
+
+// Start processing
+processor.processLogs();
