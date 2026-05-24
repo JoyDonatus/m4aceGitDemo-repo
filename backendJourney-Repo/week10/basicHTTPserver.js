3f45b6f9348f815fd6d74a9bfd6f4cb59a81a8c4
Joy Donatus <donatus.joy@outlook.com>
2026-05-24 12:22:24 +0100

    practicing routing

--- backendJourney-Repo/week10/basicHTTPserver.js
+++ backendJourney-Repo/week10/basicHTTPserver.js
@@ -1,64 +1,17 @@
-//Basic HTTP Server - 
-// Create a simple HTTP server that responds to requests with appropriate headers and status codes
 
+// Create a simple HTTP server that responds to requests with appropriate headers and status codes
 
 const http = require('http');
 
-const server = http.createServer((req, res) => {
-
-    // Home route
-    if(req.url === '/' && req.method === 'GET') {
-
-        res.writeHead(200, {
-            'Content-Type': 'text/plain'
-        });
-
-        res.end('Welcome Home');
-
-    }
-
-    // About route
-    else if(req.url === '/about' && req.method === 'GET') {
-
-        res.writeHead(200, {
-            'Content-Type': 'application/json'
-        });
-
-        res.end(JSON.stringify({
-            message: 'About Page'
-        }));
-
-    }
-
-    // Page not found
-    else {
-
-        res.writeHead(404, {
-            'Content-Type': 'text/plain'
-        });
-
-        res.end('404 Page Not Found');
-
-    }
-
+const servers = http.createServer((request, response) => {
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.write("Hello Worlds!");
+    response.end();
 });
 
-server.listen(3000, () => {
+servers.listen(3000, () => {
     console.log('Server running on port 3000');
 });
 
-
-//practising
-const app = require(`express`)();
-const PORT = 8080;
-
-app.get(`/tshirt`, (req, res) => {
-    res.status(200).send({ tshirt: "Cotton T-Shirt" , size: 'large'});
-});
-
-app.listen(
-    PORT,
-    () => console.log(`Server is running on http://localhost:${PORT}`)
-)
 
 
--- backendJourney-Repo/week10/routingAndHTTPmethods.js
+++ backendJourney-Repo/week10/routingAndHTTPmethods.js
@@ -1,3 +1,15 @@
 //Routing and HTTP Methods -
 //  Implement routing for different paths and HTTP methods, 
 // handle route parameters and query strings
+
+import http from 'http';
+import url from 'url';
+
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    var q = url.parse(req.url, true).query; // Parse the query string
+    var txt = q.year + " " + q.month; // Get year and month from query string
+    res.end(txt); // Display the result
+}).listen(8080, () => {
+    console.log('Server running at http://localhost:8080/');
+});
