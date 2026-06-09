b7048b6fecb646e02370b09faaf7dfc0900ef57c
Joy Donatus <donatus.joy@outlook.com>
2026-06-09 23:43:34 +0100

    assignment

---
+++ assignment/jobapp.js
@@ -0,0 +1,67 @@
+
+//Assignment 1: Create CRUD endpoints for job applications:
+// POST /jobs - add a new application (company, role, date applied, status)
+    // GET /jobs - list all applications
+    // GET /jobs/:id - get a single application
+    // PUT /jobs/:id - update details or status
+    // DELETE /jobs/:id - remove an application
+
+    const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+let jobApplications = [
+    { id: 1, company: "Google", role: "Frontend Developer", dateApplied: "2026-06-01", status: "Interviewing" },
+    { id: 2, company: "Stripe", role: "Backend Engineer", dateApplied: "2026-06-04", status: "Applied" }
+];
+
+app.post('/jobs', (req, res) => {
+    const company = req.body.company;
+    const role = req.body.role;
+    const dateApplied = req.body.dateApplied;
+    const status = req.body.status;
+
+    const newId = jobApplications.length + 1;
+
+    const newApplication = {
+        id: newId,
+        company: company,
+        role: role,
+        dateApplied: dateApplied,
+        status: status
+    }
+
+    jobApplications.push(newApplication);
+
+
+       
+
+})
+
+app.get('/', (req, res) => {
+    res.status(200).json({message:"Welcome to the Joy's Job Application"});
+});
+
+//GET /jobs - list all applications
+
+app.get('/jobs', (req, res) => {
+    res.status(200).json(jobApplications); //listing all applications
+});
+
+//GET /jobs/:id - get a single application
+
+app.get('/jobs/:id', (req, res) => {
+    const jobId = parseInt(req.params.id); // converting the id to an integer
+    const job = jobApplications.find(u => u.id === jobId); 
+    
+    if (job) {
+        res.status(200).json(job);
+    } else {
+        res.status(404).json({ status: 'error', message: 'Job application not found' });
+    }
+});
+
+app.listen(3000, () => {
+    console.log(`Server is running on http://localhost:3000`);
+});
